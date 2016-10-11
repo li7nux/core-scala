e2249d1650a7c79ae770d17705894feb31793705
junhui chai <junhui chai>
2016-10-11 11:46:38 +0800

    [add] first commit

---
+++ src/main/scala/club/craftcoder/App.scala
@@ -0,0 +1,9 @@
+package club.craftcoder
+
+object App {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+
+}
---
+++ src/test/scala/club/craftcoder/AppTest.scala
@@ -0,0 +1,14 @@
+package club.craftcoder
+
+import org.junit.Assert._
+import org.junit._
+
+@Test
+class AppTest {
+
+  @Test
+  def testOK() = assertTrue(true)
+
+}
+
+
